3e0a2f89d2f1f73a35709860252016e9d0d8c9ad
aliyuska <kfasklfkas@mail.ru>
2025-04-29 11:21:48 +0500

    Первый коммит

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Меню:");
+        Console.WriteLine("1. Ввести A");
+        Console.WriteLine("2. Ввести B");
+        Console.WriteLine("3. Операция +");
+        Console.WriteLine("4. Операция -");
+        Console.WriteLine("5. Операция *");
+        Console.WriteLine("6. Операция /");
+    }
+}
